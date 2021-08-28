22df2a0dcf7bd0a57381908b814f4907306784ab
John Fuquene <jfuquene@email.wm.edu>
2021-08-27 21:27:57 -0400

    music notes are not working

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function playNote(){
     document.addEventListener("keydown", (e) => {
         console.log(e.key)
         const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+        console.log(audio)
         if(!audio) return;///
         audio.play()
     })
 }
+playNote()
