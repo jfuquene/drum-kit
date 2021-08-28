0e68b2026b8f6a49755e7afe06701a0d297a6a1c
John Fuquene <jfuquene@email.wm.edu>
2021-08-27 21:04:22 -0400

    new sounds folder

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.key)
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    if(!audio) return;///
+    audio.play()
+})
