d2d032982ec5bca92596654bce41eeef8a46ba22
John Fuquene <jfuquene@email.wm.edu>
2021-08-27 21:07:01 -0400

    pushed code into function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 
-document.addEventListener("keydown", (e) => {
-    console.log(e.key)
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
-    if(!audio) return;///
-    audio.play()
-})
+function playNote(){
+    document.addEventListener("keydown", (e) => {
+        console.log(e.key)
+        const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+        if(!audio) return;///
+        audio.play()
+    })
+}
