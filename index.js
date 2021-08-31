dd82cf9565fe26f0bff78ee17dfb043ce3500db0
John Fuquene <jfuquene@email.wm.edu>
2021-08-30 23:27:13 -0400

    new functions

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
 
-function playNote(){
-    document.addEventListener("keydown", (e) => {
-        console.log(e.key)
-        const audio = document.querySelector(`audio[data-key="${e.key}"]`)
-        console.log(audio)
-        if(!audio) return;///
-        audio.play()
-    })
-}
-playNote()
+ function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`div[data-key="${e.key}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
